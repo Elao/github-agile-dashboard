401bf2d0caa2d19f809a2e62db56b7e980a8a4c0
Thomas Jarrand <thomas.jarrand@gmail.com>
2017-06-07 09:38:47 +0200

    Updated doc

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 
-const stdout = require('child_process').execSync('git -C . config --get remote.origin.url').toString();
-const result = new RegExp('git@github.com:(.+)\\/(.+)\\.git', 'ig').exec(stdout);
+const { execSync } = require('child_process');
+const remote = execSync('git -C . config --get remote.origin.url').toString();
+const result = new RegExp('git@github.com:(.+)\\/(.+)\\.git', 'ig').exec(remote);
 const { owner, repo, user, password } = require('minimist')(process.argv.slice(2), {
     default: {
         owner: result ? result[1] : null,
         repo: result ? result[2] : null,
-        user: process.env.USER,
+        user: execSync('git config --global github.user').toString() || process.env.USER,
         password: process.env.GITHUB_PERSONAL_TOKEN,
     },
     alias: {
