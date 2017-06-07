8f1e7a7ae9fb860c54412cab5fc2c5af70c9f0fd
tom32i <thomas.jarrand@gmail.com>
2017-06-07 08:37:21 +0200

    Adding default values to the CLI

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-new (require('./src/GithubAgileDashboard'))(...process.argv.slice(2));
+const stdout = require('child_process').execSync('git -C . config --get remote.origin.url').toString();
+const result = new RegExp('git@github.com:(.+)\\/(.+)\\.git', 'ig').exec(stdout);
+const { owner, repo, user, password } = require('minimist')(process.argv.slice(2), {
+    default: {
+        owner: result ? result[1] : null,
+        repo: result ? result[2] : null,
+        user: process.env.USER,
+        password: process.env.GITHUB_PERSONAL_TOKEN,
+    },
+    alias: {
+        o: 'owner',
+        r: 'repo',
+        u: 'user',
+        p: 'password',
+        t: 'password',
+        token: 'password',
+    }
+});
+new (require('./src/GithubAgileDashboard'))(owner, repo, user, password);
