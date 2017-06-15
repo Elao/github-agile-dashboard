efe6f6e16d8a58d7b7134013819734f4bb1f64d7
Thomas Jarrand <thomas.jarrand@gmail.com>
2017-06-15 13:44:58 +0200

    Fixed remote parser

--- gad.js
+++ gad.js
@@ -4,7 +4,7 @@ const { execSync } = require('child_process');
 const { homedir } = require('os');
 
 function lookup(command) { try { return execSync(command).toString().trim(); } catch (error) { return ''; } }
-function remote(url) { return new RegExp('git@github.com:(.+)\\/(.+)\\.git', 'ig').exec(url) || [null, null]; }
+function remote(url) { return (new RegExp('git@github.com:(.+)\\/(.+)\\.git', 'ig').exec(url) || new Array(3).fill(null)).slice(1); }
 
 const [defaultOwner, defaultRepo] = remote(lookup('git -C . config --get remote.origin.url'));
 const { owner, repo, user, password, cacheDir, _: commands} = require('minimist')(process.argv.slice(2), {
